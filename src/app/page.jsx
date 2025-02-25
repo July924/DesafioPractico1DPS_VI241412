658ff9eb2570561903459d7dd51eeb458bae0a71
Julissa <128512140+July924@users.noreply.github.com>
2025-02-25 15:58:44 -0600

    Atualizacion de diseño page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,6 +8,17 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <header className={styles.encabezado}><h1>Venta de Boletos - Cine</h1>
+       <h3 className={styles.letra}>Selecciona tus asientos</h3>
+    <div className={styles.asientos}>  
+        <div className={styles.disponibilidad}>
+        <label>Ocupados:</label>
+        <button className={styles.boton__negro}></button>
+        </div>
+        <div className={styles.disponibilidad}>
+        <label>Disponibles:</label>
+        <button className={styles.boton__verde }></button>
+        </div>
+          </div>
     <div className={styles.contenedor__principal}>
       <div className={styles.contenedor}>
       <SelectorSala />
